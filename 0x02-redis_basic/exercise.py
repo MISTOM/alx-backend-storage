164b7cf76c853873012deefa0f807b18c65aec9c
MISTOM <kigardetom2001@gmail.com>
2022-04-28 11:20:41 +0300

    retrieve data from Redis Data Storage with their original types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 '''
 
 
-from typing import Union
+from typing import Callable, Union
 import redis
 import uuid
 
@@ -16,11 +16,28 @@ class Cache:
         '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
-    
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Store a value in Redis Data Storage and return the Key
         '''
         rand_key = str(uuid.uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieve data from Redis Data Storage
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string from Redis Data Storage
+        '''
+        return self.get(key, lambda x: x.decode('utf8'))
+
+    def get_int(self, key: str) -> str:
+        '''Retrieves a integer from Redis Data Storage
+        '''
+        return self.get(key, lambda x: int(x))
