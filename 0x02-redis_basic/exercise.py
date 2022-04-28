d03832d4d27638b2095e02223c4141bdf3a877db
MISTOM <kigardetom2001@gmail.com>
2022-04-28 11:33:00 +0300

    create replay method" Displays the call history of a Cache class' method.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 '''
 
 
-from typing import Callable, Union
+from typing import Callable, Union, Any
 from functools import wraps
 import redis
 import uuid
@@ -40,6 +40,31 @@ def call_history(method: Callable) -> Callable:
     return invoker
 
 
+def replay(fn: Callable) -> None:
+    '''Displays the call history of a Cache class' method.
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
 class Cache:
     '''Represents an object for caching data in Redis data storage
     '''
@@ -49,6 +74,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Store a value in Redis Data Storage and return the Key
         '''
